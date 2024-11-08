60d3e4bb30b02ef81803c1be1ee951da284d50b9
adam <adamtreistera@gmail.com>
2024-11-08 11:31:10 -0800

    split AttributeStore out from TreeNode

--- CytegeistLibrary/Util/TreeNode.swift
+++ CytegeistLibrary/Util/TreeNode.swift
@@ -11,38 +11,6 @@ import Foundation
 //public typealias AttributeStore = [String:String]
 
 
-public class AttributeStore: Codable, Hashable
-{
-    public init () {
-        dictionary = [String:String]()
-    }
-
-    public static func == (lhs: AttributeStore, rhs: AttributeStore) -> Bool {
-        if lhs.dictionary.count != rhs.dictionary.count { return false}
-            
-        for pair in lhs.dictionary {
-            if rhs.dictionary[pair.key] != pair.value {return false}
-        }    
-        return true
-    }
-    public func hash(into hasher: inout Hasher)
-    {
-        
-    }
-    public var dictionary: [String:String]
-    
-    
-    public func xml() -> String {
-        
-        var str = "<Keywords>\n"
-        for pair in dictionary {
-            str.append("<Keyword name=\(pair.key) value=\(pair.value)\n")
-        }
-        return str + "</Keywords>\n"
-    }
-
-}
-
 public class TreeNode
 {
     public var value = ""
